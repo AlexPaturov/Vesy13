b63d5926d297ccd50aaad092a75fa213d0b67faa
AlexPaturov <alexey.paturov@gmail.com>
2026-06-09 12:05:16 +0300

    refactor: centralize password dialog theme

--- Forms/PasswordDialog.cs
+++ Forms/PasswordDialog.cs
@@ -12,8 +12,23 @@ public partial class PasswordDialog : Form
     protected override void OnLoad(EventArgs e)
     {
         base.OnLoad(e);
-        _lblPrompt.Font   = UiFonts.Medium;
+        ApplyTheme();
+    }
+
+    private void ApplyTheme()
+    {
+        BackColor = UiColors.AppBackground;
+        _lblPrompt.Font = UiFonts.Medium;
+        _lblPrompt.ForeColor = UiColors.TextPrimary;
         _txtPassword.Font = UiFonts.SubHeader;
+        _txtPassword.BackColor = UiColors.InputBack;
+        _txtPassword.ForeColor = UiColors.InputFore;
+        _btnOk.Font = UiFonts.BodyBold;
+        _btnOk.BackColor = UiColors.PrimaryAction;
+        _btnOk.ForeColor = UiColors.TextOnDark;
+        _btnCancel.Font = UiFonts.Body;
+        _btnCancel.BackColor = UiColors.NeutralAction;
+        _btnCancel.ForeColor = UiColors.TextPrimary;
     }
 
     private void TxtPassword_KeyDown(object? sender, KeyEventArgs e)
